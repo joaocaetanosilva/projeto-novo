bbc72d729e8f18f25c4e27b0729be6f2154718e8
JoaoCaetanoSilva <joaocaetanosilva1234@gmail.com>
2023-09-23 10:16:24 -0300

    Primeiro commit

---
+++ src/main/java/br/com/muffin/projetonovo/ProjetoNovoApplication.java
@@ -0,0 +1,13 @@
+package br.com.muffin.projetonovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoNovoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoNovoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/muffin/projetonovo/ProjetoNovoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.muffin.projetonovo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoNovoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
